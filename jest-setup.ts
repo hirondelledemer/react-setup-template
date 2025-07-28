08f173ecb8aa5b302a705811f15a692ea3a2fffc
Meda Šiaudvytytė <meda.siaudvytyte@nordsec.com>
2025-07-28 13:02:22 +0300

    upgrade dependencies

--- jest-setup.ts
+++ jest-setup.ts
@@ -3,6 +3,5 @@ import '@testing-library/jest-dom/jest-globals';
 // Polyfill for TextEncoder/TextDecoder in Jest environment
 if (typeof global.TextEncoder === 'undefined') {
   const { TextEncoder, TextDecoder } = require('util');
-  global.TextEncoder = TextEncoder;
-  global.TextDecoder = TextDecoder;
+  Object.assign(global, { TextEncoder, TextDecoder });
 }
